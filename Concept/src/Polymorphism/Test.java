3d29d56b49e967a9cde6fe395fe2e89c57109ed8
AshishN1993 <GitHub@8007>
2019-03-23 16:18:06 +0530

    first

---
+++ Concept/src/Polymorphism/Test.java
@@ -0,0 +1,18 @@
+package Polymorphism;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Honda h = new Honda();
+		h.run();
+		
+		Bike b = new Honda();
+		b.run();
+		
+
+	}
+
+}
