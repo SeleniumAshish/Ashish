88cef3eb34a27d38efec12c4d28abc293782dee2
AshishN1993 <GitHub@8007>
2019-03-23 16:00:11 +0530

    Second Commit

---
+++ Concept/src/Polymorphism/Bike.java
@@ -0,0 +1,11 @@
+package Polymorphism;
+
+public class Bike {
+	
+	
+	public void run(){
+		
+		System.out.println("Bike is Running");
+	}
+
+}
---
+++ Concept/src/Polymorphism/Honda.java
@@ -0,0 +1,10 @@
+package Polymorphism;
+
+public class Honda extends Bike {
+	
+	public void run(){
+		System.out.println("My Honda Bike is running");
+		
+	}
+
+}
